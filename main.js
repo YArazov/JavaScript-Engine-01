2219ab7153f9213072eca043401e0110ce38a3ca
SeikohMurakami <seikoh.murakami@student.cga.school>
2024-01-31 23:22:55 +0900

    Create rectangle

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ import {Circle} from './circle.js';
 const canv = document.getElementById("canvas");
 const ctx = canv.getContext("2d");
 
-const renderer = new Renderer(canv, ctx);
-
+const renderer = new Renderer(canv, ctx);   //objects and classes
+//new rederer creates a new object instance of the class Renderer, it inherits all the functionlaity of Renderer class
 //MAIN LOOP
 function updateAndDraw() {
 
@@ -14,6 +14,6 @@ function updateAndDraw() {
     //draw objects
     renderer.clearFrame();
     renderer.drawCircle(circle, "black");
-
+renderer.drawRect();    //calling the method, using it
 }
 let renderInterval = setInterval(updateAndDraw, 1000 / 60);
--- renderer.js
+++ renderer.js
@@ -5,7 +5,7 @@ export class Renderer {
         this.objects = objects;
     }
 
-    drawCircle(circle, strokeColor, fillColor) {
+    drawCircle(circle, strokeColor, fillColor) {    //classes and methods
         this.ctx.beginPath();
         this.ctx.arc(circle.position.x, circle.position.y, circle.radius, 0, Math.PI*2, true);
         this.ctx.closePath();
@@ -17,6 +17,16 @@ export class Renderer {
         this.ctx.lineWidth = 3;
         this.ctx.stroke();
     }
+
+    drawRect() {    //declaring a method, not using it here
+        
+      
+          this.ctx.fillRect(25, 25, 100, 100);
+         this.ctx.clearRect(45, 45, 60, 60);
+          this.ctx.strokeRect(50, 50, 50, 50);
+    
+      }
+      
 
     clearFrame() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
