3827d7e90e18c32ff00b81873ade12f5808088a2
Ken1806school <157724853+Ken1806school@users.noreply.github.com>
2024-02-01 17:29:07 +0800

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -17,6 +17,29 @@ export class Renderer {
         this.ctx.lineWidth = 3;
         this.ctx.stroke();
     }
+    drawRect(rect, strokeColor, fillColor) {
+        this.ctx.save();
+        this.ctx.translate(rect.position.x, rect.position.y);
+        if (fillColor) {
+            this.ctx.fillStyle = fillColor;
+            this.ctx.fillRect(
+                - rect.width/2,
+                - rect.height/2,
+                rect.width,
+                rect.height,
+            );
+        }
+        this.ctx.strokeStyle = strokeColor;
+        this.ctx.lineWidth = 3;
+        this.ctx.strokeRect(
+            - rect.width/2,
+            - rect.height/2,
+            rect.width,
+            rect.height,
+        );
+        this.ctx.restore();
+    }
+
 
     clearFrame() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
