94c4d64ca0c71d126b41c79e41fd4ab6b0402ac0
MidiaD <darya.midia@student.cga.school>
2024-02-21 23:14:56 -0500

    collision detection algorithm for circles

--- collisions.js
+++ collisions.js
@@ -8,7 +8,7 @@ export class Collisions {
     }
     narrowPhaseDetection(objects){
         for(let i=0; i<objects.length; i++){
-            for(let j=0; j<objects.length; j++){
+            for(let j=0; i<objects.length; j++){
             if(j>i){
                 //detect collisions
                 //circle collisions
@@ -23,6 +23,17 @@ export class Collisions {
         }
     }
     detectCollisionCircleCircle(o1,o2){
-
+        const s1 = o1.shape;
+        const s2 = o2.shape;
+        const dist = s1.position.distanceTo(s2.position);
+        if(dist < s1.radius + s2.radius){//circle collision
+            const overlap = s1.radius + s2.radius - dist;
+            const normal = s2.position.clone().subtract(s1.position).normalize();
+            this.collisions.push({
+                collidedPair:[o1,o2],
+                overlap: overlap,
+                normal: normal
+            });
+        }
     }
 }
