9c7a242398d3882075415b4ee024f87cb0d83225
MidiaD <darya.midia@student.cga.school>
2024-02-21 22:11:59 -0500

    Collisions nested for loop

--- collisions.js
+++ collisions.js
@@ -2,4 +2,27 @@ export class Collisions {
 	constructor(x, y) {
 		this.collisions = [];
 		
-	} }
+	} 
+    clearCollisions(){
+        this.collisions = [];
+    }
+    narrowPhaseDetection(objects){
+        for(let i=0; i<objects.length; i++){
+            for(let j=0; j<objects.length; j++){
+            if(j>i){
+                //detect collisions
+                //circle collisions
+                if(objects[i].shape instanceof Circle && 
+                    objects[j].shape instanceof Circle){
+                        detectCollisionCircleCircle(objects[i],objects[j]);
+                    }
+                //rectangle collisions
+
+            }
+            } 
+        }
+    }
+    detectCollisionCircleCircle(o1,o2){
+
+    }
+}
