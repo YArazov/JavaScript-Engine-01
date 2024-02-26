ac281340f8fc26ee2d01bd52b6ddfc2048371d4d
SeikohMurakami <seikoh.murakami@student.cga.school>
2024-02-26 15:19:20 +0900

    collisions nested for loop

--- collisions.js
+++ collisions.js
@@ -1,5 +1,29 @@
+import { Circle } from "./circle";
 export class Collisions {
 	constructor(x, y) {
 		this.collisions = [];
 	}
+
+    clearCollisions() {
+        this.collisions = [];
+    }
+
+    narrowPhazeDetection(objects) {
+        for(let i=0; i<objects.length; i++) {
+            for(let j=0; i<objects.length; j++) {
+                if(j>i) {
+                    //detect collisions
+                    //circle collisions
+                    if(object[i].shape instanceof Circle &&
+                        object[j].shape instanceof Circle) {
+                            this.detectCollisionCircleCircle(object[i], object[j]);
+                        }
+                    //rectangle collisions
+                }
+            }
+        }
+    }
+    detectCollisionCircleCircle(o1, o2) {
+
+    }
 }
