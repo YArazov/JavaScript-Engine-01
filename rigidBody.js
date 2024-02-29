2262e5fe4969445e7e3a0165ec7775c90f499e13
YArazov <156554298+YArazov@users.noreply.github.com>
2024-03-01 01:23:47 +0800

    rectangle vertices

--- rigidBody.js
+++ rigidBody.js
@@ -1,16 +1,21 @@
 import {Vec} from './vector.js';
+import {Rect} from './rect.js';
 
 export class RigidBody {
 	constructor(shape) {
 		this.shape = shape; //rectangle or circle object {}  
 		this.velocity = new Vec(0, 0);
-		this.angularVelocity = 100;
+		this.angularVelocity = 1;
 	}	
 
 	updateShape(dt) {
 		const ds = this.velocity.clone().multiply(dt);  //multiply v * dt = giving you displacement per frame
 		this.shape.position.add(ds);
 		this.shape.orientation += this.angularVelocity * dt;
-    } 
+		if (this.shape instanceof Rect) {
+			this.shape.updateVertices();
+			this.shape.updateAabb();
+		}
+	} 
 
 }
