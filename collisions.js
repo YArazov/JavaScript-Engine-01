252a8f69def856471800d7cd48c836dd4a4137d5
YArazov <156554298+YArazov@users.noreply.github.com>
2024-03-28 21:23:49 +0800

    circle-polygon detect on closest vertex axis

--- collisions.js
+++ collisions.js
@@ -80,6 +80,31 @@ export class Collisions {
             }
         }
         //also test for axis that is from the closest Vertex to the center of circle
+        const closestVertex = this.findClosestVertex(vertices, cShape.position);
+        axis = closestVertex.clone().subtract(cShape.position).normalize(); //axis from circle to closest vertex on polygon
+        
+        const [min1, max1] = this.projectVertices(vertices, axis);
+        const [min2, max2] = this.projectCircle(cShape.position, cShape.radius, axis);
+        if (min1 >= max2 || min2 >= max1) {
+            return;
+        }
+
+        const axisOverlap = Math.min(max2-min1, max1-min2); //find on which axis we have the smallest overlap
+        if (axisOverlap < overlap) {
+            overlap = axisOverlap;
+            normal = axis;
+        }
+
+        const vec1to2 = p.shape.position.clone().subtract(c.shape.position);  //gives correct direction for normal
+        if (normal.dot(vec1to2) < 0) { 
+            normal.invert();
+        }
+
+        this.collisions.push({
+            collidedPair: [c, p],
+            overlap: overlap,
+            normal: normal,       //direction from c1 to c2
+        });
     }
 
     projectVertices (vertices, axis) {
